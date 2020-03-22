523a368327b950df3a58b17cfc439f2244c0bb42
laloespino <Ee45Q9Q4-github>
2020-03-22 03:26:15 -0600

    Updates

---
+++ precache-manifest.106d5aacce1e93c905283e0759c0326a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11039df301046b65c9e11c64b8fe6537",
+    "url": "/LaloEspino/coronavirus-information/index.html"
+  },
+  {
+    "revision": "e3b42d29d8a0f20ee0be",
+    "url": "/LaloEspino/coronavirus-information/static/js/2.9eeeff14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LaloEspino/coronavirus-information/static/js/2.9eeeff14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba61265e797e7e901982",
+    "url": "/LaloEspino/coronavirus-information/static/js/main.0499b12f.chunk.js"
+  },
+  {
+    "revision": "71fc6dc108b9f5606749",
+    "url": "/LaloEspino/coronavirus-information/static/js/runtime-main.d87bf6ff.js"
+  }
+]);
